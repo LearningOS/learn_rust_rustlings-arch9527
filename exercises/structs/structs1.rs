21e09f6316825e781b13ddff6d8a5612b5a1a52f
arch9527 <arch10@163.com>
2022-07-08 00:18:48 +0800

    rustlings

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,16 +1,21 @@
 // structs1.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
-struct ColorClassicStruct {
+struct ColorClassicStruct<'a> {
     // TODO: Something goes here
+    name: &'a str,
+    hex: &'a str,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct<'a>(&'a str, &'a str);
 
 #[derive(Debug)]
 struct UnitStruct;
+impl UnitStruct {
+    fn new() -> &'static str {
+        "UnitStruct"
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -19,7 +24,10 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct {
+            name: "green",
+            hex: "#00FF00",
+        };
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -28,7 +36,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
+        let green = ("green", "#00FF00");
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -37,8 +45,8 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        // let unit_struct =
-        let message = format!("{:?}s are fun!", unit_struct);
+        let unit_struct = UnitStruct::new();
+        let message = format!("{}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
     }
